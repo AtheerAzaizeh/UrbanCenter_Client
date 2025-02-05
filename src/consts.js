31c205ab6edecb26ad162edf74742761ab7ea767
atheersnm@gmail.com <atheersnm@gmail.com>
2025-02-06 00:28:10 +0200

    server

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:8080'
+//export const SERVER_URL = 'http://localhost:8080'
 // export const SERVER_URL = 'http://192.168.0.199:3000'
+export const SERVER_URL = 'https://urbancenter-server.onrender.com'
